2027040065265f791345408f818bd6d18a7445c3
yeving <havit.biznes@gmail.com>
2025-10-04 17:36:35 +0200

    Add files via upload
    uploaded files

---
+++ script.js
@@ -0,0 +1,25 @@
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+
+}
+
+function clearDisplay(){
+    display.value = "";
+
+}
+
+function calculate(){
+   try{
+     display.value = eval(display.value);
+   }
+   catch(error){
+        display.value = "Error"
+   }
+   if (display.value === "6") { 
+        window.alert("wykryto szpont xdd");
+   }
+        
+   
+}
